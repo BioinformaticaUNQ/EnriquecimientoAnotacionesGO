9bed07669d4b8111219827c076952392e9706c73
ENRIQUE ALONSO <alonso.em@gmail.com>
2024-06-17 18:24:37 -0300

    creating dynamic url step 1

--- src/integraciones/blast_client.ts
+++
@@ -1,3 +0,0 @@
-import requests
-response = requests.get('https://google.com/')
-print(response)
--- src/integraciones/uniprot_client.py
+++ src/integraciones/uniprot_client.py
@@ -2,7 +2,10 @@ import requests
 class UniprotClient:
     def __init__(self):
         #first time example
-        self.url="https://rest.uniprot.org/uniprotkb/search?query=reviewed:true+AND+organism_id:9606&format=fasta"
+        self.uniprotId="Q3SXR2"
+        self.url="https://rest.uniprot.org/uniprotkb/search?query=reviewed:true+AND"
+        self.url= self.url + +"organism_id:"+self.uniprotId
+        self.url=self.url + "&format=fasta"
         
-    def get(self):
+    def getProtein(self):
         return requests.get(self.url)
