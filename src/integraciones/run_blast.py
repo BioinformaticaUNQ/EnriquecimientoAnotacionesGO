187a7cda22214754de960c5b1ff3858dd780fd8b
Lucas Alvarez <alvarezlucas2787@gmail.com>
2024-06-26 18:14:01 -0300

    outfmt json

--- src/integraciones/run_blast.py
+++ src/integraciones/run_blast.py
@@ -40,11 +40,14 @@ def add_database(filename, database):
 def run_query(protein, database, blast_args):
     command_path = os.path.join(blast_path, "blastp")
     
-    db_path = os.path.join(current_path, "integraciones/db", database)
+    if ("-remote" in blast_args):
+        db_path = database
+    else:
+        db_path = os.path.join(current_path, "integraciones/db", database)
     protein_path = os.path.join(current_path, "integraciones/proteins", protein + ".fasta")
 
     args = (command_path, "-db", db_path,
-            "-query", protein_path)
+            "-query", protein_path, "-outfmt", "13")
     
     for arg in blast_args:
         args = args + (arg,)
@@ -57,11 +60,14 @@ def run_query(protein, database, blast_args):
 def show_help():
     command_path = os.path.join(blast_path, "blastp")
 
-    args = (command_path, "-h")
+    args = (command_path, "-help")
     popen = subprocess.Popen(args, stdout=subprocess.PIPE, cwd="./")
     popen.wait()
     print_output(popen)
 
+
+def run_remote_query(protein, database):
+    pass
 
 
 # command_path, "-db", "src/integraciones/db/" + database,
